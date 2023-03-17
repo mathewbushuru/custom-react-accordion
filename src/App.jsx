5848444febeb786e0b7db6b8218945e1a09638e1
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-03-16 20:03:11 -0700

    refactor out AccordionItem

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,29 @@ import { Accordion } from "./components/Accordion";
 
 import "./App.css";
 
+const DUMMY_DATA = [
+  {
+    title: "Section 1",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur distinctio cumque beatae laudantium numquam assumenda fugiat aut nisi eveniet quis molestiae, consequuntur, debitis, hic maxime ipsa vel non nihil nulla.",
+  },
+  {
+    title: "Section 2",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur distinctio cumque beatae laudantium numquam assumenda fugiat aut nisi eveniet quis molestiae, consequuntur, debitis, hic maxime ipsa vel non nihil nulla.",
+  },
+  {
+    title: "Section 3",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur distinctio cumque beatae laudantium numquam assumenda fugiat aut nisi eveniet quis molestiae, consequuntur, debitis, hic maxime ipsa vel non nihil nulla.",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       Accordion
-      <Accordion />
+      <Accordion data={DUMMY_DATA} />
     </div>
   );
 }
--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -2,30 +2,38 @@ import React, { useState } from "react";
 
 import "./Accordion.css";
 
-const DUMMY_DATA = {
-  title: "Section 1",
-  content:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur distinctio cumque beatae laudantium numquam assumenda fugiat aut nisi eveniet quis molestiae, consequuntur, debitis, hic maxime ipsa vel non nihil nulla.",
-};
 
-export const Accordion = () => {
-  const { title, content } = DUMMY_DATA;
-  const [isActive, setIsActive] = useState(false);
+
+export const Accordion = ({ data }) => {
   return (
     <>
       <h1>Accordion Demo</h1>
       <div className="accordion">
-        <div className="accordion-item">
-          <div
-            className="accordion-title"
-            onClick={() => setIsActive((prevState) => !prevState)}
-          >
-            <div>{title}</div>
-            <div>{isActive ? "-" : "+"}</div>
-          </div>
-          {isActive && <div className="accordion-content">{content}</div>}
-        </div>
+        {data.map((data) => (
+          <AccordionItem
+            key={crypto.randomUUID()}
+            title={data.title}
+            content={data.content}
+          />
+        ))}
       </div>
     </>
   );
 };
+
+function AccordionItem({ title, content }) {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="accordion-item">
+      <div
+        className="accordion-title"
+        onClick={() => setIsActive((prevState) => !prevState)}
+      >
+        <div>{title}</div>
+        <div>{isActive ? "-" : "+"}</div>
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </div>
+  );
+}
